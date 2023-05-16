e92960a656134e9ef4fc41ae0a8b01b47e81331a
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-16 11:15:14 +0900

    Book.unapplyの実装

--- src/main/scala/UnapplyChallenge.scala
+++ src/main/scala/UnapplyChallenge.scala
@@ -1,4 +1,10 @@
 object UnapplyChallenge {
   class Book(private val title: String)
-}
 
+  object Book {
+    def unapply(obj: Any): Option[String] = obj match {
+      case book: Book => Some(book.title)
+      case _ => None
+    }
+  }
+}
